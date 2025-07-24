84c5b93d1aa369ec9c9728812934a292bb4d0a20
alireza <alirezueffati@gmail.com>
2025-07-24 12:49:40 +0330

    final touch

---
+++ main.py
@@ -0,0 +1,72 @@
+from tkinter import *
+import settigns
+import utils
+from cell import Cell
+
+root = Tk()
+# override the settinge of the window
+root.configure(bg='black')
+root.geometry(f'{settigns.WIDTH}x{settigns.HEIGHT}')
+root.title("minesweeper")
+root.resizable(False, False)
+
+top_frame = Frame(
+    root,
+    bg='black',
+    width=settigns.WIDTH,
+    height=utils.height_prct(25),
+)
+top_frame.place(x=0, y=0)
+
+game_title = Label(
+    top_frame, 
+    bg="black",
+    fg="white",
+    text="Minesweeper Game",
+    font=("", 48)
+)
+
+game_title.place(
+    x=utils.width_prct(25), y=0
+)
+
+left_frame = Frame(
+    root,
+    bg='black',
+    width=utils.width_prct(25),
+    height=utils.height_prct(75),
+)
+left_frame.place(x=0, y=utils.height_prct(25))
+
+
+center_frame = Frame(
+    root,
+    bg='black',
+    width=utils.width_prct(75),
+    height=utils.height_prct(75),
+)
+center_frame.place(
+    x=utils.width_prct(25),
+    y=utils.height_prct(25),
+)
+
+for x in range(settigns.GRID_SIZE):
+    for y in range(settigns.GRID_SIZE):
+        c = Cell(x, y)
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(
+                column=x, row=y
+        )
+
+Cell.create_cell_count_label(left_frame)
+Cell.cell_count_label_object.place(
+        x=0,
+        y=0,
+)
+
+Cell.randomize_mines()
+
+
+
+# run the window
+root.mainloop()
---
+++ utils.py
@@ -0,0 +1,7 @@
+import settigns
+
+def height_prct(percentage):
+    return (settigns.HEIGHT / 100) * percentage
+
+def width_prct(percentage):
+    return (settigns.WIDTH / 100) * percentage
